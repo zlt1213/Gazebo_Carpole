346ce7a06d792d941a0c79f7f10b9c85ace2a099
luz211 <zlt1213@gmail.com>
2018-07-06 01:25:08 -0700

    energy forming code added

--- src/joint_angle_test.cpp
+++ src/joint_angle_test.cpp
@@ -4,35 +4,47 @@
 #include "sensor_msgs/JointState.h"
 
 
-
 ros::Subscriber joint_sub;
 sensor_msgs::JointState msg;
 
 
 std_msgs::Float64 msg_pub;
-ros::Publisher joint_pub;
+// ros::Publisher joint_pub;
+
+ros::Publisher joint_eff_pub;
+sensor_msgs::JointState msg_eff_pub;
+
+// ==== define the paramters for the cart-pole problem
+float m_car = 1.0;
+float m_pole = 3.0;
+float l = 0.6;
+float g = 9.8;
+float e_target = m_pole * g * l;
+float k_e = 1.0;
+float k_p = 1.0;
+float k_d = 1.0;
+
 
 void chatterCallback(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  ros::Time c_time = ros::Time::now();
-  double secs = c_time.toSec();
 
-  ROS_INFO("pos:[%f]  vel:[%f]  effort:[%f]", msg->position[0], msg->velocity[0], secs );
-
-  msg_pub.data = sin(10 * secs) * 0.3;
-  joint_pub.publish(msg_pub);
-
-
-  // if(msg->position[0] > 0.2 || msg->position[0] <-0.3){
-  //   msg_pub.data = 0.0;
-  //   joint_pub.publish(msg_pub);
-  //
-  // }else{
-  //   msg_pub.data  = msg->position[0] + 0.01;
-  //   joint_pub.publish(msg_pub);
-  // }
+  // calculate the current energy of the pendulum
+  float pole_theta_d = 3.141 / 2.0-msg->velocity[0];
+  float pole_theta = 3.141 / 2.0 -msg->position[0];
+  float car_x = msg->position[1];
+  float car_x_dot = msg->velocity[1];
 
 
+  float e_pendu = 1.0 / 2.0 * m_pole * l * l * pole_theta_d * pole_theta_d - m_pole * g * l * cos(pole_theta);
+  float e_diff = e_pendu - e_target;
+  float out_eff = k_e * pole_theta_d * cos(pole_theta) * e_diff - k_p * car_x - k_d * car_x_dot;
+
+
+  msg_pub.data = out_eff;
+  joint_eff_pub.publish(msg_pub);
+
+
+  ROS_INFO("Energy[%f]", e_pendu);
 
 
 }
@@ -45,7 +57,8 @@ int main(int argc, char **argv)
   ros::NodeHandle joint_sub_h;
 
   joint_sub = joint_sub_h.subscribe("/carpole/joint_states", 1000, chatterCallback);
-  joint_pub = joint_sub_h.advertise<std_msgs::Float64>("/carpole/joint1_position_controller/command", 1000);
+  // joint_pub = joint_sub_h.advertise<std_msgs::Float64>("/carpole/joint1_position_controller/command", 1000);
+  joint_eff_pub = joint_sub_h.advertise<std_msgs::Float64>("/carpole/joint1_effort_controller/command", 100);
   ros::spin();
   return 0;
 }
